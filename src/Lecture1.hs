d70f936346b1fd5e47a0de6b0840186679c9cef6
Krishna Padmasola <kpadmasola@users.noreply.github.com>
2022-02-01 10:24:20 +0530

    Solutions for Lecture 1 (#1)
    * lecture 1 exercises
    
    * Update src/Lecture1.hs
    
    Co-authored-by: Dmitrii Kovanikov <kovanikov@gmail.com>
    
    * Update src/Lecture1.hs
    
    Co-authored-by: Dmitrii Kovanikov <kovanikov@gmail.com>
    
    * Added a new testcase for lecture one substring test (#38)
    
    * Clarify about treasure in Lecture 2
    
    * Add tests on infinite lists for Lecture 2 (#43)
    
    * Add more Lecture 2 tests (#46)
    
    * Clarify that hard challenges are extra (#47)
    
    * Set UTF-8 encoding in tests (#49)
    
    * addressed review comments
    
    Co-authored-by: Dmitrii Kovanikov <kovanikov@gmail.com>
    Co-authored-by: Aleksandra Koen <al.v.cohen@gmail.com>

--- src/Lecture1.hs
+++ src/Lecture1.hs
@@ -35,6 +35,7 @@ module Lecture1
 its behaviour, possible types for the function arguments and write the
 type signature explicitly.
 -}
+makeSnippet :: Int -> [Char] -> [Char]
 makeSnippet limit text = take limit ("Description: " ++ text) ++ "..."
 
 {- | Implement a function that takes two numbers and finds sum of
@@ -50,7 +51,8 @@ Explanation: @sumOfSquares 3 4@ should be equal to @9 + 16@ and this
 is 25.
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-sumOfSquares x y = error "TODO!"
+sumOfSquares :: Int -> Int -> Int
+sumOfSquares x y = x * x + y * y
 
 {- | Implement a function that returns the last digit of a given number.
 
@@ -63,7 +65,8 @@ sumOfSquares x y = error "TODO!"
 
 -}
 -- DON'T FORGET TO SPECIFY THE TYPE IN HERE
-lastDigit n = error "lastDigit: Not implemented!"
+lastDigit :: Int -> Int
+lastDigit n = abs n `mod` 10
 
 {- | Write a function that takes three numbers and returns the
 difference between the biggest number and the smallest one.
@@ -77,7 +80,11 @@ and 1 is the smallest, and 7 - 1 = 6.
 Try to use local variables (either let-in or where) to implement this
 function.
 -}
-minmax x y z = error "TODO"
+minmax :: Int -> Int -> Int -> Int
+minmax x y z = biggest - smallest
+    where
+        biggest  = max x (max y z)
+        smallest = min x (min y z)
 
 {- | Implement a function that takes a string, start and end positions
 and returns a substring of a given string from the start position to
@@ -94,7 +101,9 @@ start position can be considered as zero (e.g. substring from the
 first character) and negative end position should result in an empty
 string.
 -}
-subString start end str = error "TODO"
+subString :: Int -> Int -> [Char] -> [Char]
+subString start end str = take numChars (drop start str)
+    where numChars = end - max 0 start + 1
 
 {- | Write a function that takes a String — space separated numbers,
 and finds a sum of the numbers inside this string.
@@ -104,7 +113,8 @@ and finds a sum of the numbers inside this string.
 
 The string contains only spaces and/or numbers.
 -}
-strSum str = error "TODO"
+strSum :: String -> Int
+strSum str = sum (map read (words str))
 
 {- | Write a function that takes a number and a list of numbers and
 returns a string, saying how many elements of the list are strictly
@@ -119,4 +129,14 @@ and lower than 6 elements (4, 5, 6, 7, 8 and 9).
 
 🕯 HINT: Use recursion to implement this function.
 -}
-lowerAndGreater n list = error "TODO"
+lowerAndGreater :: Int -> [Int] -> [Char]
+lowerAndGreater n = go 0 0
+    where
+        go :: Int -> Int -> [Int] -> [Char] 
+        go numGreater numLower xs
+            | null xs = show (n :: Int) ++ " is greater than " ++ show (numGreater :: Int) ++ " elements and lower than " ++ show (numLower :: Int) ++ " elements"
+            | otherwise = if n > head xs
+                          then go (numGreater + 1) numLower (tail xs)
+                          else if n == head xs
+                               then go numGreater numLower (tail xs)
+                               else go numGreater (numLower + 1) (tail xs)
